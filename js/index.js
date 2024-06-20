b57017735ea1877c3ddf3bd01986c2c68a9c1c04
bek-211203-coder <bek406197@gmail.com>
2024-06-20 08:21:09 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,248 @@
+// massiv ga doir masalalr
+
+// masala 1
+// function Color(col1, col2, col3) {;
+//     return [col1, col2, col3]
+//   }
+
+//   const Array = Color( red , blue, black);
+//   console.log(Array);
+
+// masala 2
+// function Number(num1, num2, num3) {
+//     return [num1, num2, num3];
+//   }
+
+//   const Array = Number(5, 10, 15);
+//   console.log(Array);
+
+// masala 3
+// function num (arr) {
+//     return [arr[0], arr[arr.length - 1]];
+//   }
+//   let a = [1, 2, 3, 4, 5];
+//   let res = num(a);
+//   console.log(res);
+
+// masala 5
+// function vrabel() {
+//   return arr;
+// }
+// let arr = ["matiz", "tiko", "olma", "anor"];
+// arr.push("malibu");
+// console.log(vrabel(arr));
+
+// masala 6
+// function vrabel() {
+//   return arr;
+// }
+// let arr = ["matiz", "tiko", "olma", "anor"];
+// arr.pop();
+// console.log(vrabel(arr));
+
+// masala 7
+// function vrabel(a) {
+//   return a.length;
+// }
+
+// let arr = ["matiz", "tiko", "olma", "anor"];
+// console.log(vrabel(arr));
+
+// masala 8
+// function vrabel() {
+//   return arr;
+// }
+// let arr = ["matiz", "tiko", "olma", "anor"];
+// arr.unshift('banan')
+// console.log(vrabel(arr));
+
+// masala 9
+// function vrabel() {
+//     return arr;
+//   }
+//   let arr = ["matiz", "tiko", "olma", "anor"];
+//   arr.shift()
+//   console.log(vrabel(arr));
+
+//  masal 11
+// function sum (arr1, arr2) {
+//     return arr1.includes(arr2);
+// }
+// let res = [1, 2, 3, 4, 5];
+// console.log(sum(res, 3));
+
+// masala 13
+// function sum (arr1, arr2) {
+//     return arr1.indexOf(arr2);
+// }
+// let res = [1, 2, 3, 4, 5];
+// console.log(sum(res, 3));
+
+
+// masala 14
+// function sum (arr) {
+//     return arr
+// }
+// let res = [1, 2, 3, 4, 5];
+//      res.splice(2,3)
+// console.log(sum(res));
+
+// masala 15
+// function del(array) {
+//     arr.pop();
+//     arr.pop();
+//     return array;
+// }
+// let arr = [1, 2, 3, 4, 5];
+// console.log( del (arr));
+
+
+
+
+// masala 33
+// function sum(array1, array2) {
+//     return array1.concat(array2);
+// }
+// let array1 = [1, 2, 3];
+// let array2 = [4, 5, 6];
+// console.log(sum(array1, array2));
+
+
+// masala 35 
+// function sorted(array) {
+//     return array.sort((a, b) => a - b);
+// }
+// let res = [5, 2, 9, 1, 5, 6];
+// console.log(sorted(res));
+
+
+
+// Massiv metodlariga oid masalalar.
+// Quyidagi masalarni funksiyalar yordamida ishlang.
+
+
+// masala 1
+// const cars = ['matiz', 'damas', 'tico' , 'nexia', 'tico', 'damas' ];
+// cars.push("malibu");
+// console.log(cars);
+
+// masaala 2
+// const cars = ['matiz', 'damas', 'tico' , 'nexia', 'tico', 'damas' ];
+// cars.pop();
+// console.log(cars);
+
+// masala 3
+// const cars = ['matiz', 'damas', 'tico' , 'nexia', 'tico', 'damas' ];
+// cars.shift();
+// console.log(cars);
+
+
+// masala 4
+// const cars = ['matiz', 'damas', 'tico' , 'nexia', 'tico', 'damas' ];
+// cars.unshift("taxo");
+// console.log(cars);
+
+
+// masala 5
+// const cars = ['matiz', 'damas', 'tico' , 'nexia', 'tico', 'damas' ];
+// console.log(cars.includes("damas"));
+// console.log(cars);
+
+
+// masala 6
+// const cars = ['matiz', 'damas', 'tico' , 'nexia', 'tico', 'damas' ];
+// console.log(cars.indexOf("tico"));
+// console.log(cars);
+
+
+
+// masala 7
+// const cars = ['matiz', 'damas', 'tico' , 'nexia', 'tico', 'damas' ];
+// console.log(cars.lastIndexOf("tico"));
+// console.log(cars);
+
+
+
+// masala 8
+// const cars = ['matiz', 'damas', 'tico' , 'nexia', 'tico', 'damas' ];
+// cars.splice();
+// console.log(cars);
+
+
+
+// masala 9
+// function arr(cars, a) {
+//     cars.push(a);
+//     return cars;
+// }
+// const cars = ['matiz', 'damas', 'tico' , 'nexia', 'tico', 'damas' ];
+// let a = 6;
+// let res = arr(cars, a);
+// console.log(res);
+
+// masala 10
+// function arr(cars) {
+//     cars.pop();
+//     return cars;
+// }
+// const cars = ['matiz', 'damas', 'tico' , 'nexia', 'tico', 'damas' ];
+// let res = arr(cars);
+// console.log(res);
+
+// masala 11
+// function arr(cars) {
+//     cars.shift();
+//     return cars;
+// }
+// const cars = ['matiz', 'damas', 'tico' , 'nexia', 'tico', 'damas' ];
+// let res = arr(cars);
+// console.log(res);
+
+
+// masala 12
+// function arr(cars) {
+//     cars.unshift();
+//     return cars;
+// }
+// const cars = ['matiz', 'damas', 'tico' , 'nexia', 'tico', 'damas' ];
+// let res = arr(cars);
+// console.log(res);
+
+
+// masala 13
+// function arr(cars) {
+//     cars.includes("damas")
+//     return cars;
+// }
+// const cars = ['matiz', 'damas', 'tico' , 'nexia', 'tico', 'damas' ];
+// let res = arr(cars);
+// console.log(res);
+
+// masala 14
+// function arr(cars) {
+//     cars.indexOf("tico")
+//     return cars;
+// }
+// const cars = ['matiz', 'damas', 'tico' , 'nexia', 'tico', 'damas' ];
+// let res = arr(cars);
+// console.log(res);
+
+// masala 15
+// function arr(cars) {
+//     cars.lastIndexOf("tico")
+//     return cars;
+// }
+// const cars = ['matiz', 'damas', 'tico' , 'nexia', 'tico', 'damas' ];
+// let res = arr(cars);
+// console.log(res);
+
+
+// masala 16
+// function arr(cars) {
+//     cars.splice()
+//     return cars;
+// }
+// const cars = ['matiz', 'damas', 'tico' , 'nexia', 'tico', 'damas' ];
+// let res = arr(cars);
+// console.log(res);
+
